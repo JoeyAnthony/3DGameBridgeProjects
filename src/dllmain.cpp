7a3057e39d856505e3e1d52c3a163df7d233a031
Joey van Hoek <joey@dimenco.eu>
2022-07-29 10:36:22 +0200

    Initial commit Contains a buildable project with cmake

---
+++ src/dllmain.cpp
@@ -0,0 +1,33 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include "pch.h"
+static void on_present(reshade::api::command_queue* queue, reshade::api::swapchain* swapchain, const reshade::api::rect* source_rect, const reshade::api::rect* dest_rect, uint32_t dirty_rect_count, const reshade::api::rect* dirty_rects)
+{
+    // ...
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        // Call 'reshade::register_addon()' before you call any other function of the ReShade API.
+        // This will look for the ReShade instance in the current process and initialize the API when found.
+        if (!reshade::register_addon(hModule))
+            return FALSE;
+        // This registers a callback for the 'present' event, which occurs every time a new frame is presented to the screen.
+        // The function signature has to match the type defined by 'reshade::addon_event_traits<reshade::addon_event::present>::decl'.
+        // For more details check the inline documentation for each event in 'reshade_events.hpp'.
+        reshade::register_event<reshade::addon_event::present>(&on_present);
+        break;
+    case DLL_PROCESS_DETACH:
+        // Optionally unregister the event callback that was previously registered during process attachment again.
+        reshade::unregister_event<reshade::addon_event::present>(&on_present);
+        // And finally unregister the add-on from ReShade (this will automatically unregister any events and overlays registered by this add-on too).
+        reshade::unregister_addon(hModule);
+        break;
+    }
+    return TRUE;
+}
