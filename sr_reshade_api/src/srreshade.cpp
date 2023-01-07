feb84b06aec28e4159debbcc07fbf254a5382bf9
Joey <jvanhoek@hotmail.com>
2023-01-07 20:28:04 +0100

    Copying dll to executable location

--- sr_reshade_api/src/srreshade.cpp
+++ sr_reshade_api/src/srreshade.cpp
@@ -1,12 +1,12 @@
 #include "srreshade.h"
+#include <iostream>
 
 namespace srreshade {
 
     SR_RESHADE_API void subscribe_to_pocess_events()
     {
-
+        std::cout << "Hello World" << std::endl;
     }
-
     SR_RESHADE_API void unsubscribe_from_process_events()
     {
 
