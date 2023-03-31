9f3d0ce5d16004978fbb025208f6607dd9d6bacd
BramTeurlings <bram@dimenco.eu>
2023-03-31 12:06:07 +0200

    Implemented adaptive latency code for DX11
    untested, needs implementation for DX12 as well

--- gamebridge_reshade/src/igraphicsapi.h
+++ gamebridge_reshade/src/igraphicsapi.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "pch.h"
 
+
+enum LatencyModes { framerateAdaptive, latencyInFrames };
+
 class IGraphicsApi {
 public:
     virtual void draw_debug_overlay(reshade::api::effect_runtime* runtime) = 0;
@@ -12,4 +15,18 @@ public:
     virtual ~IGraphicsApi() = default;
 
     virtual void on_destroy_swapchain(reshade::api::swapchain *swapchain) {};
+
+    //Latency settings
+    // The latency mode must be set before attempting to set the latency with "set_latency_in_frames" or "set_latency_framerate_adaptive".
+    virtual void set_latency_mode(LatencyModes mode) = 0;
+    virtual LatencyModes get_latency_mode() = 0;
+
+    // The two methods below return true if the latency was succesfully set, they return false if their current latency mode does not permit them to set the latency.
+
+    // This method should only be called once, after which it will use the maximum framerate of the monitor in use to determine the latency of the eye tracker.
+    // This does NOT look at the current framerate, use this when you are able to consistently reach your monitor's maximum refresh rate.
+    virtual bool set_latency_in_frames(int numberOfFrames) = 0;
+
+    // This method must be called every frame with the current frametime.
+    virtual bool set_latency_framerate_adaptive(int frametimeInMicroseconds) = 0;
 };
